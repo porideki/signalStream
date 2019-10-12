48770ba32b2285dc318af29af9f8bd6b65c8929e
porideki <porideki@gmail.com>
2019-10-12 19:17:37 +0900

    socketクラスファイルを作成

---
+++ Assets/Scripts/porideki/parts/InputSocket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+
+namespace Assets.Scripts.porideki.parts {
+    public class InputSocket<T> {
+
+        private List<OutputSocket<T>> outputSockets;
+        private ReactiveProperty<T> valuePorperty;
+
+
+
+        public InputSocket() {
+
+            
+
+        }
+
+    }
+
+}
