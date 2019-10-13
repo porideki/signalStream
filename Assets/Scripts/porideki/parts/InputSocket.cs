b589fe1e0cba1de6f5e387982460052a2c7de551
porideki <porideki@gmail.com>
2019-10-13 21:44:36 +0900

    socketクラス・gateクラスを実装

--- Assets/Scripts/porideki/parts/InputSocket.cs
+++ Assets/Scripts/porideki/parts/InputSocket.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
@@ -7,15 +8,34 @@ using UniRx;
 namespace Assets.Scripts.porideki.parts {
     public class InputSocket<T> {
 
-        private List<OutputSocket<T>> outputSockets;
-        private ReactiveProperty<T> valuePorperty;
+        private ReactiveProperty<T> valueProperty;
+        //購読のみ
+        public IReadOnlyReactiveProperty<T> readOnlyValueProperty {
+            get { return this.valueProperty; }
+        }
+
+        public InputSocket(T initialValue) {
+
+            this.valueProperty = new ReactiveProperty<T>();
+            this.Set(initialValue);
+
+        }
+
+        public T Get() {
+            return this.valueProperty.Value;
+        }
 
 
+        public void Set(T value) {
+            this.valueProperty.Value = value;
+        }
 
-        public InputSocket() {
-
-            
-
+        /// <summary>
+        /// ChangeListener的扱い
+        /// </summary>
+        /// <param name="action"></param>
+        public void Subscribe(Action<T> action) {
+            this.valueProperty.Subscribe(action);
         }
 
     }
--- Assets/Scripts/test/PropertyTest.cs
+++ Assets/Scripts/test/PropertyTest.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using Assets.Scripts.porideki.gates;
+using Assets.Scripts.porideki.parts;
 
 public class PropertyTest : MonoBehaviour {
 
     public Transform boxTranceform;
-    public Text text;
+    public Text positionText;
+    public Text addText;
 
     void Awake() {
 
+        var divGate = new DivisionGate();
+        InputSocket<double> inputSocket = new InputSocket<double>(0);
+        inputSocket.readOnlyValueProperty.Subscribe(v => this.addText.text = v.ToString());
+        divGate.resultSocket.inputSockets.Add(inputSocket);
+
         boxTranceform
             .ObserveEveryValueChanged(transform => transform.position)
-            .Subscribe(position => this.text.text = position.ToString());
+            .Subscribe(position => {
+                this.positionText.text = position.ToString();
+                divGate.dividendSocket.Set(position.x);
+                divGate.divisorSocket.Set(position.y);
+            });
 
     }
 
