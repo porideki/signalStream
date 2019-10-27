64b0bd21b2b5547f49c4d700e5f01806ed173249
porideki <porideki@gmail.com>
2019-10-27 23:27:24 +0900

    カメラ位置をドラッグで移動させられるよう設定

---
+++ Assets/Scripts/CircuitEditor/CameraDrag.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraDrag : MonoBehaviour, IDragHandler {
+
+    public EditorCameraController editorCameraController;
+
+    public void OnDrag(PointerEventData pointerEventData) {
+
+        if(pointerEventData.button == PointerEventData.InputButton.Middle) {
+
+            Vector3 delta = pointerEventData.delta;
+            delta.z = 0;
+            //Screen座標スケールに変換
+            delta.x /= Screen.width;
+            delta.y /= Screen.height;
+            //World画面サイズ
+            var leftTopPosition = Camera.main.ScreenToWorldPoint(Vector3.zero);
+            var rightBottomPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            var worldScreenSize = rightBottomPosition - leftTopPosition;
+            //World座標系に変換
+            delta.x *= worldScreenSize.x;
+            delta.y *= worldScreenSize.y;
+            //反転
+            delta.x *= -1;
+            delta.y *= -1;
+
+            //カメラ・背景移動
+            this.editorCameraController.MoveCamera(delta);
+
+        }
+
+    }
+    
+}
---
+++ Assets/Scripts/CircuitEditor/EditorCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorCameraController : MonoBehaviour {
+
+    public Transform backgroundTransform;
+
+    public void MoveCamera(Vector3 delta) {
+
+        //カメラ移動
+        Camera.main.transform.position += delta;
+        //背景移動
+        var cameraPos = Camera.main.transform.position;
+        cameraPos.z = 0;
+        this.backgroundTransform.position = cameraPos;
+
+    }
+
+}
